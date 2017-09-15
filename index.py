38aec365d2a675340ca197fdd653b88ac9999138
IDerr <ibrahim@derraz.fr>
2017-09-16 01:41:06 +0200

    first try

---
+++ index.py
@@ -0,0 +1,24 @@
+from plyer import notification
+from dealabs import Dealabs
+import time
+
+
+def main():
+    dealabs_client = Dealabs()
+    old = dealabs_client.get_deals_hot()
+    old_time = int(time.time())
+    notification_data={}
+    while True:
+        old = dealabs_client.get_deals_hot()
+        for hot in old['data']:
+            if hot['hot_reached_on']['timestamp'] > old_time:
+                notification_data['title'] = 'Hot Deal'
+                notification_data['message'] = '{0} {1}'.format(hot['title'], hot['price'])
+                notification.notify(**notification_data)
+        old_time = int(time.time())
+        time.sleep(20)
+
+
+if __name__ == '__main__':
+    main()
+
